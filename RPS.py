af0f2235346f939f29887a0b6111a4a5ab811eb2
dslynn247 <161959888+dslynn247@users.noreply.github.com>
2024-03-02 11:25:59 -0500

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,4 +1,44 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+def play(player1, player2, num_games, verbose=False):
+    p1_prev_play = ""
+    p2_prev_play = ""
+    results = {"p1": 0, "p2": 0, "tie": 0}
+
+    for _ in range(num_games):
+        p1_play = player1(p2_prev_play)
+        p2_play = player2(p1_prev_play)
+
+        if p1_play == p2_play:
+            results["tie"] += 1
+            winner = "Tie."
+        elif (p1_play == "P" and p2_play == "R") or (
+                p1_play == "R" and p2_play == "S") or (p1_play == "S"
+                                                       and p2_play == "P"):
+            results["p1"] += 1
+            winner = "Player 1 wins."
+        elif p2_play == "P" and p1_play == "R" or p2_play == "R" and p1_play == "S" or p2_play == "S" and p1_play == "P":
+            results["p2"] += 1
+            winner = "Player 2 wins."
+
+        if verbose:
+            print("Player 1:", p1_play, "| Player 2:", p2_play)
+            print(winner)
+            print()
+
+        p1_prev_play = p1_play
+        p2_prev_play = p2_play
+
+    games_won = results['p2'] + results['p1']
+
+    if games_won == 0:
+        win_rate = 0
+    else:
+        win_rate = results['p1'] / games_won * 100
+
+    print("Final results:", results)
+    print(f"Player 1 win rate: {win_rate}%")
+
+    return (win_rate)
 
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
@@ -8,3 +48,4 @@ def player(prev_play, opponent_history=[]):
         guess = opponent_history[-2]
 
     return guess
+
